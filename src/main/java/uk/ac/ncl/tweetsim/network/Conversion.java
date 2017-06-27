637e35519a9fbefb326690a1887bbc2f7f8f3c44
JonoCX <jonathan.carlton@hotmail.com>
2017-06-27 15:32:42 +0100

    added untested conversion class

--- src/main/java/uk/ac/ncl/tweetsim/network/Conversion.java
+++ src/main/java/uk/ac/ncl/tweetsim/network/Conversion.java
@@ -1,12 +1,16 @@
 package uk.ac.ncl.tweetsim.network;
 
 import br.les.opus.twitter.domain.Tweet;
+import br.les.opus.twitter.domain.TweetClassification;
 import br.les.opus.twitter.domain.TwitterUser;
+import br.les.opus.twitter.repositories.TweetClassificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import uk.ac.ncl.botnetwork.domain.Connection;
 import uk.ac.ncl.botnetwork.domain.User;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.transaction.Transactional;
+import java.util.*;
 
 /**
  * @author Jonathan Carlton
@@ -31,9 +35,22 @@ public class Conversion
     }
 
     public static List<Tweet> tweetConvert(
-            List<uk.ac.ncl.botnetwork.domain.Tweet> list)
+            List<uk.ac.ncl.botnetwork.domain.Tweet> list,
+            Map<Long, TweetClassification> classificationMap)
     {
         List<Tweet> result = new ArrayList<>();
+
+        Tweet tweet;
+        for (uk.ac.ncl.botnetwork.domain.Tweet t : list) {
+            tweet = new Tweet();
+            tweet.setText(t.getText());
+            tweet.setClassification(classificationMap.get(t.getClassificationId()));
+
+            // set random ID
+            tweet.setId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE);
+
+            result.add(tweet);
+        }
 
         return result;
     }
@@ -42,6 +59,38 @@ public class Conversion
             List<TwitterUser> users, List<Connection> connections)
     {
         List<TwitterUser> result = new ArrayList<>();
+
+        Map<TwitterUser, List<Long>> followingMap = new HashMap<>();
+        for (TwitterUser u : users) {
+            followingMap.put(u, new ArrayList<>());
+        }
+
+        for (TwitterUser u : users) {
+            List<Long> temp = new ArrayList<>();
+            for (Connection conn : connections) {
+                if (conn.getOrigin().getTwitterId().equals(u.getId())) {
+                    temp.add(conn.getDestination().getTwitterId());
+                }
+            }
+            followingMap.put(u, temp);
+        }
+
+        for (Map.Entry<TwitterUser, List<Long>> m : followingMap.entrySet()) {
+            List<Long> list = m.getValue();
+            TwitterUser origin = m.getKey();
+
+            for (TwitterUser user : users) {
+                if (list.contains(user.getId())) {
+                    for (Long l : list) {
+                        if (l.equals(user.getId())) {
+                            origin.getFollowing().add(user);
+                        }
+                    }
+                }
+            }
+
+            result.add(origin);
+        }
 
         return result;
     }
